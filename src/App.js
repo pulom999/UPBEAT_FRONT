9946fc5515f1459e7f199ec6b8bb7c8490c87256
pulom999 <pulom999@gmail.com>
2023-03-12 20:57:50 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainMenu from './components/MainMenu';
+import Hexmap from './components/Hexmap';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MainMenu} />
+          <Route path="/hexmap" component={Hexmap} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/MainMenu.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Hexmap from './Hexmap';
+
+
+
+function MainMenu() {
+  const [showHowToPlay, setShowHowToPlay] = useState(false);
+  const [showHexMap, setShowHexMap] = useState(false);
+  const history = useHistory();
+
+  function handleHowToPlayClick() {
+    setShowHowToPlay(true);
+  }
+
+  function handleHowToPlayClose() {
+    setShowHowToPlay(false);
+  }
+
+  function handlePlayClick() {
+    setShowHexMap(true);
+    history.push('/hexmap');
+    window.location.href = '/hexmap.html';
+  }
+
+  return (
+    <div>
+      <h1>Welcome to UPBEAT</h1>
+      <div className="button-container">
+        <button className="play-btn" onClick={handlePlayClick}>
+          Play
+        </button>
+        <button className="htp-btn" onClick={handleHowToPlayClick}>
+          How to Play
+        </button>
+      </div>
+
+      {showHowToPlay && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>How to Play</h2>
+            <p>Use the arrow keys to move the map.</p>
+            <p>Use the left mouse to interact.</p>
+            <button onClick={handleHowToPlayClose}>Close</button>
+          </div>
+        </div>
+      )}
+       {showHexMap && <Hexmap />}
+    </div>
+  );
+}
+
+export default MainMenu;
